2ffe51245a14c4050c3e45c0d340e9f9bc128ec2
hyenee <hyein0522>
2021-10-02 15:18:25 +0900

    [Add]

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ def main():
                     } </style>""", unsafe_allow_html=True)
 
     # Select Box for the model
-    st.sidebar.image('./fig/logo.png', width=150) #use_column_width='auto'
+    st.sidebar.image('./fig/logo.png', width=150)
     model_name = st.sidebar.selectbox("Model", ("t5", "bart", "gpt2"))
-    dataset = st.sidebar.selectbox("Dataset", ("Weather"))  
+    #dataset = st.sidebar.selectbox("Dataset", ("Weather"))  
     num_return_sequences = st.sidebar.slider("Number of return sentences", 0, 100)
 
     st.sidebar.text('')
